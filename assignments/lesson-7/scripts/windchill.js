f5a4fffc4a5b9a26442fafd87ee8aedbeb12e75d
clwinward <clwinward@gmail.com>
2018-02-24 17:07:34 -0700

    add windchill calculation

--- assignments/lesson-7/scripts/windchill.js
+++ assignments/lesson-7/scripts/windchill.js
@@ -1,16 +1,16 @@
-f = 35.74 + 0.6215 t - 35.75 s0.16 + 0.4275 t s0.16 where f is the wind chill factor in Fahrenheit, t is the air average temperature in Fahrenheit, and s is the wind speed in miles per hour.
-Hint: See the Pen JS Basic I/O
+function computeWindChill() {
+    // Input - get input
+    var tempInput = document.getElementById("avgT").value;
+    var speedInput = document.getElementById("windS").value;
+
+    // Processing - f = 35.74 + 0.6215 * t - 35.75 * s^0.16 + 0.4275 * t s^0.16
+    var t = parseInt(tempInput); // OK - parse string to number
+    var s = parseInt(speedInput); // OK - parse string to number
+    var f = 35.74 + (0.6215 * t) - (35.75 * Math.pow(s, 0.16)) + (0.4275 * t) * Math.pow(s, 0.16);
+
+    // Output - output to the div and round the number value to two decimal places
+    document.getElementById("output").innerHTML = "The wind chill factor is <strong>" + f.toFixed(2) + "</strong>!";
+}
 
 
-/* HTML for input
-<section>
-    <div class="form">
-    Enter the average temperature in Fahrenheit as an integer: <input type="text" id="avgT" maxlength="3" size="1">
-    Enter the wind speed in miles per hour: <input type="text" id="windS" maxlength="3" size="1">
 
-    <input type="submit" onclick="computeWindChill()" value="Run Calculation">
-
-    <div id="output">calculated windchill</div>
-    </div>
-</section>
-*/

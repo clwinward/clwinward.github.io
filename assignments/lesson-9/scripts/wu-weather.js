6a52c58f965c360306dcb86987afe8238e84938e
clwinward <clwinward@gmail.com>
2018-03-10 20:09:49 -0700

    lesson 9 working except formatting

--- assignments/lesson-9/scripts/wu-weather.js
+++ assignments/lesson-9/scripts/wu-weather.js
@@ -1,7 +1,13 @@
-Use an XMLRequest object to request the JSON content (see example below) of the the Weather Underground API and get the following data:
+var requestURL = 'https://api.wunderground.com/api/aab9c2fad2e8a298/forecast/geolookup/conditions/q/MN/Franklin.json';
+var request = new XMLHttpRequest();
+request.open('GET', requestURL, true);
+request.send();
 
-The current weather string, e.g., Partly Cloudy, from conditions.
-The current temperature in Fahrenheit from conditions.
-The current wind speed in miles per hour from conditions.
-The image icon representing the current weather from conditions.
-The text description forecast for the most current period in Fahrenheit, e.g., Partly cloudy. Lows overnight in the upper 70s.
+request.onload = function() {
+    var franklinWeather = JSON.parse(request.responseText);
+    document.getElementById('currentWeather').innerHTML = franklinWeather.current_observation.weather;
+    document.getElementById('currentTemp').innerHTML = franklinWeather.current_observation.temp_f;
+    document.getElementById('currentWind').innerHTML = franklinWeather.current_observation.wind_mph;
+    document.getElementById('weatherIcon').src = franklinWeather.current_observation.icon_url;
+    document.getElementById('currentForecast').innerHTML = franklinWeather.forecast.txt_forecast.forecastday[0].fcttext;
+}

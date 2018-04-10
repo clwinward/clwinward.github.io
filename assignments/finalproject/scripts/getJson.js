1112806df55a6540fb4f3a62fcbf4d44b550d2cc
clwinward <clwinward@gmail.com>
2018-04-10 13:23:17 -0700

    updating json and sales page working

--- assignments/finalproject/scripts/getJson.js
+++ assignments/finalproject/scripts/getJson.js
@@ -19,10 +19,17 @@ request.open('GET', requestURL, true);
 request.send();
 
 request.onload = function() {
-   var bikedata = JSON.parse(request.responseText);
-   document.getElementById('blueName').innerHTML = bikedata.bikes[0].name;
-   document.getElementById('bluegreenName').innerHTML = bikedata.bikes[2].name;
-   document.getElementById('redName').innerHTML = bikedata.bikes[1].name;
+    var bikedata = JSON.parse(request.responseText);
+    document.getElementById('blueName').innerHTML = bikedata.bikes[0].name;
+    document.getElementById('bluegreenName').innerHTML = bikedata.bikes[2].name;
+    document.getElementById('redName').innerHTML = bikedata.bikes[1].name;
 
+    document.getElementById('bluePrice').innerHTML = bikedata.bikes[0].price;
+    document.getElementById('bluegreenPrice').innerHTML = bikedata.bikes[2].price;
+    document.getElementById('redPrice').innerHTML = bikedata.bikes[1].price;
+
+    document.getElementById('blueFeatures1').innerHTML = bikedata.bikes[0].features[0];
+    document.getElementById('bluegreenFeatures1').innerHTML = bikedata.bikes[2].features[0];
+    document.getElementById('redFeatures1').innerHTML = bikedata.bikes[1].features[0];
 }
 

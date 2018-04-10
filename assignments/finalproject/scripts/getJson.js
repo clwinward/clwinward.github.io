b5ef02dfce48d57701fa27dab826e72629c1e93b
clwinward <clwinward@gmail.com>
2018-04-09 22:42:48 -0700

    working on getJSON not working

--- assignments/finalproject/scripts/getJson.js
+++ assignments/finalproject/scripts/getJson.js
@@ -1,26 +1,13 @@
-var requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+function load() {
+    var bikedata = JSON.parse(bikes);
 
-var request = new XMLHttpRequest();
-request.open('GET', requestURL, true);
+    document.getElementById('blueName').innerHTML = bikedata[0].name;
 
-request.send();
 
-request.onload = function() {
-    var townData = JSON.parse(request.responseText);
+    document.getElementById('bluegreenName').innerHTML = bikedata[1].name;
 
-    document.getElementById('franklinMotto').innerHTML = townData.towns[0].motto;
-    document.getElementById('franklinYear').innerHTML = townData.towns[0].yearFounded;
-    document.getElementById('franklinPopulation').innerHTML = townData.towns[0].currentPopulation;
-    document.getElementById('franklinRainfall').innerHTML = townData.towns[0].averageRainfall;
 
-    document.getElementById('greenvilleMotto').innerHTML = townData.towns[1].motto;
-    document.getElementById('greenvilleYear').innerHTML = townData.towns[1].yearFounded;
-    document.getElementById('greenvillePopulation').innerHTML = townData.towns[1].currentPopulation;
-    document.getElementById('greenvilleRainfall').innerHTML = townData.towns[1].averageRainfall;
+    document.getElementById('redName').innerHTML = bikedata[2].name;
 
-    document.getElementById('springfieldMotto').innerHTML = townData.towns[3].motto;
-    document.getElementById('springfieldYear').innerHTML = townData.towns[3].yearFounded;
-    document.getElementById('springfieldPopulation').innerHTML = townData.towns[3].currentPopulation;
-    document.getElementById('springfieldRainfall').innerHTML = townData.towns[3].averageRainfall;
 }
 

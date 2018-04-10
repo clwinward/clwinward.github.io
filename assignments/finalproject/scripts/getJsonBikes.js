72ab43d7c1d4480ea6e5d6b88fcfcd0df2350826
clwinward <clwinward@gmail.com>
2018-04-10 14:50:25 -0700

    services page upload images and content

--- assignments/finalproject/scripts/getJsonBikes.js
+++ assignments/finalproject/scripts/getJsonBikes.js
@@ -1,17 +1,4 @@
-/*request.onload function() {
-    var bikedata = JSON.parse(data/bikeInfo.json);
-
-    document.getElementById('blueName').innerHTML = bikedata[0].name;
-
-
-    document.getElementById('bluegreenName').innerHTML = bikedata[1].name;
-
-
-    document.getElementById('redName').innerHTML = bikedata[2].name;
-
-}*/
-
-var requestURL = 'https://clwinward.github.io/assignments/finalproject/data/bikeInfo.json';
+var requestURL = 'https://clwinward.github.io/assignments/finalproject/data/tours.json';
 
 var request = new XMLHttpRequest();
 request.open('GET', requestURL, true);
@@ -19,8 +6,8 @@ request.open('GET', requestURL, true);
 request.send();
 
 request.onload = function() {
-    var bikedata = JSON.parse(request.responseText);
-    document.getElementById('blueName').innerHTML = bikedata.bikes[0].name;
+    var tourdata = JSON.parse(request.responseText);
+    document.getElementById('blueName').innerHTML = tourdata.tour[0].name;
     document.getElementById('bluegreenName').innerHTML = bikedata.bikes[2].name;
     document.getElementById('redName').innerHTML = bikedata.bikes[1].name;
 

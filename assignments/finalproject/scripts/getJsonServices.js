8ea9feb60fd2ce40972215dbcb5f87206ec3c9ff
clwinward <clwinward@gmail.com>
2018-04-10 23:10:54 -0700

    update contact page, services table works

--- assignments/finalproject/scripts/getJsonServices.js
+++ assignments/finalproject/scripts/getJsonServices.js
@@ -7,19 +7,16 @@ request.onload = function() {
 
     var repairdata = JSON.parse(request.responseText);
 
-    document.getElementById('repair1Name').innerHTML = repairdatadata.services[0].name;
-    document.getElementById('repair2Name').innerHTML = repairdatadata.services[1].name;
-    document.getElementById('repair3Name').innerHTML = repairdatadata.services[2].name;
-    document.getElementById('repair4Name').innerHTML = repairdatadata.services[3].name;
-    document.getElementById('repair5Name').innerHTML = repairdatadata.services[4].name;
+    document.getElementById('repair1Name').innerHTML = repairdata.services[0].name;
+    document.getElementById('repair2Name').innerHTML = repairdata.services[1].name;
+    document.getElementById('repair3Name').innerHTML = repairdata.services[2].name;
+    document.getElementById('repair4Name').innerHTML = repairdata.services[3].name;
+    document.getElementById('repair5Name').innerHTML = repairdata.services[4].name;
 
-    document.getElementById('repair1Price').innerHTML = repairdatadata.services[0].cost;
-    document.getElementById('repair2Price').innerHTML = repairdatadata.services[1].cost;
-    document.getElementById('repair3Price').innerHTML = repairdatadata.services[2].cost;
-    document.getElementById('repair4Price').innerHTML = repairdatadata.services[3].cost;
-    document.getElementById('repair5Price').innerHTML = repairdatadata.services[4].cost;
-
-    document.getElementById('').innerHTML = repairdatadata.services[0].includes[];
-
+    document.getElementById('repair1Price').innerHTML = repairdata.services[0].cost;
+    document.getElementById('repair2Price').innerHTML = repairdata.services[1].cost;
+    document.getElementById('repair3Price').innerHTML = repairdata.services[2].cost;
+    document.getElementById('repair4Price').innerHTML = repairdata.services[3].cost;
+    document.getElementById('repair5Price').innerHTML = repairdata.services[4].cost;
 }
 
